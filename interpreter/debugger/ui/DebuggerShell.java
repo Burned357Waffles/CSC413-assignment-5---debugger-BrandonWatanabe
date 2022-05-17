54892cb860d608d9a96ac70441e7b7da00710757
burned357waffles <burned357waffles@gmail.com>
2022-05-16 23:28:51 -0700

    print source file

--- interpreter/debugger/ui/DebuggerShell.java
+++ interpreter/debugger/ui/DebuggerShell.java
@@ -2,9 +2,27 @@ package interpreter.debugger.ui;
 
 import interpreter.debugger.Debugger;
 
-public class DebuggerShell {
-  public DebuggerShell(Debugger debugger) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class DebuggerShell {
+  public DebuggerShell(Debugger debugger, String sourceFileName) {
+    try
+    {
+      File codeFile = new File(sourceFileName);
+      Scanner inputFile =  new Scanner(codeFile);
+      while (inputFile.hasNext())
+      {
+        String nextLine = inputFile.nextLine();
+        System.out.println(nextLine);
+      }
+      inputFile.close();
+    }
+    catch (FileNotFoundException e)
+    {
+      System.out.println("File " + sourceFileName + " not found." );
+    }
   }
 
   public DebuggerCommand prompt() {
